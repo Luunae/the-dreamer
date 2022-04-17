c23aaea2c0eacd5966af9fd6c303d5a667be3dce
Luna <49705655+Luunae@users.noreply.github.com>
2022-04-16 21:50:56 -0700

    version 0.1.0
    Functional Quote feature. Needs tidying.

--- main.py
+++ main.py
@@ -3,8 +3,11 @@
 # This bot uses the discord.py library.
 import os
 import discord
+from discord import Guild
+from discord.abc import GuildChannel
 from discord.ext import commands
 from dotenv import load_dotenv
+from datetime import datetime
 import validators
 
 load_dotenv(verbose=True)
@@ -19,44 +22,60 @@ description = (
 intents = discord.Intents.default()
 intents.members = True
 
-bot = commands.Bot(command_prefix="!", description=description, intents=intents)
+bot = commands.Bot(
+    command_prefix="!",
+    description=description,
+    intents=intents,
+    allowed_mentions=discord.AllowedMentions().none(),
+)
+
+
+def url_is_valid(link: str):
+    return validators.url(link)
+
+
+def replace_text_with_quoted_text(message: discord.Message):
+    message.content = "> " + message.content
+    message.content = message.content.replace("\n", "\n> ")
+    return message
+
+
+def compose_quote(message: discord.Message):
+    message = replace_text_with_quoted_text(message)
+    quote: str = f'__@{message.author}__ in **#{message.channel}** at <Timestamps included in the "Lucid figures out how the fuck datetimes work" DLC.>:\n'
+    quote += message.content
+    # if message.edited_at:
+    #     quote += f"\n\nEdited at <t:{datetime.timestamp(message.edited_at)}>"
+    return quote
 
 
 # Defines a function that checks if the string passed is a valid discord message URL.
-# The function takes a message and a guild ID as arguments and returns a boolean.
-def is_message_url(message: str, reference_guild_id) -> bool: # reference_guild_id is derived from message.guild of the original message context.
-    # If the message is not a valid URL, return False.
-    if not validators.url(message):
-        return False
+# The function takes a message and a guild ID as arguments and either returns False or a message object.
+async def get_message_from_url(link: str):
+    # Split the link on slashes.
+    split_link = link.split("/")
+    # Get server ID, channel ID, and message ID from the link, and store them as integers.
+    server_id = int(split_link[4])
+    channel_id = int(split_link[5])
+    message_id = int(split_link[6])
 
-    # First split the url on slashes.
-    url_split = message.split("/")
-    # Then, unpack the guild ID, channel ID, and message IDs, as ints, from url_split.
-    guild_id, channel_id, message_id = (
-        int(url_split[4]),
-        int(url_split[5]),
-        int(url_split[6]),
-    )
-    # Create a guild object from the guild ID.
-    guild_object = bot.get_guild(guild_id)
-    if reference_guild_id != guild_object:
+    # Use the bot to get the guild object.
+    guild: Guild = bot.get_guild(server_id)
+    # Use the bot to get the channel object.
+    channel: GuildChannel = guild.get_channel(channel_id)
+    if not isinstance(channel, discord.TextChannel):
         return False
-    # Create a channel object from the channel ID.
-    channel_object = guild_object.get_channel(channel_id)
-    # Create a message object from the message ID.
-    message_object = channel_object.fetch_message(message_id)
-    # Get the message from the message object.
-    # Make sure to handle possible exceptions.
-    # Possible exceptions include: NotFound, Forbidden, and HTTPException.
+    # Get the message object from the channel.
+    message: discord.Message = await channel.fetch_message(message_id)
     try:
-        message_content = message_object.content
+        message_content: str = message.content
     except discord.NotFound:
         return False
     except discord.Forbidden:
         return False
     except discord.HTTPException:
         return False
-    return True
+    return message
 
 
 @bot.event
@@ -77,7 +96,22 @@ async def on_message(message):
     # If the message is from a user, and the message starts with the prefix,
     # then run the function.
     if message.content.startswith("http"):
-        print(is_message_url(message.content, message.guild))
+        if url_is_valid(message.content):
+            message_from_url = await get_message_from_url(message.clean_content)
+            if message_from_url:
+                quote = compose_quote(message_from_url)
+                if len(message_from_url.attachments) > 1:
+                    attachments_to_send = []
+                    # For each attacment in message_from_url, convert it to a file then add that file to attachments_to_send.
+                    for attachment in message_from_url.attachments:
+                        attachments_to_send.append(await attachment.to_file())
+                    await message.channel.send(quote, files=attachments_to_send)
+                elif len(message_from_url.attachments) == 1:
+                    # Convert the only attachment from message_from_url to a file, then add that file to attachment_to_send.
+                    attachment_to_send = await message_from_url.attachments[0].to_file()
+                    await message.channel.send(quote, file=attachment_to_send)
+                else:
+                    await message.channel.send(content=quote)
 
 
 bot.run(BOT_TOKEN)
