f57e3457375648edb220a539ffa452b1b1cc87de
Luna <49705655+Luunae@users.noreply.github.com>
2022-06-04 19:47:44 -0700

    version 3.0.2
    Refactored Teleport, fucked with help info.

--- main.py
+++ main.py
@@ -4,9 +4,10 @@
 import os
 import discord
 from discord.ext import commands
+from discord.ext.commands import DefaultHelpCommand
 from dotenv import load_dotenv
 import quote_unroll
-import teleport
+import utilities
 import bot
 
 load_dotenv(verbose=True)
@@ -15,11 +16,35 @@ BOT_TOKEN = str(os.getenv("DISCORD_TOKEN"))
 description = """A simple utility bot."""
 intents = discord.Intents.default()
 intents.members = True
+
+
+class MyHelpCommand(DefaultHelpCommand):
+    def __init__(self, **options):
+        super().__init__(**options)
+        self.no_category = options.pop('no_category', 'Uncategorized')
+
+    def add_indented_commands(self, commands, *, heading, max_size=None):
+        if not commands:
+            return
+
+        self.paginator.add_line(heading)
+        max_size = max_size or self.get_max_size(commands)
+
+        get_width = discord.utils._string_width
+        for command in commands:
+            name = command.name
+            width = max_size - (get_width(name) - len(name))
+            entry = '{0}{1:<{width}} {2}'.format(self.indent * ' ', name, command.short_doc, width=width)
+            self.paginator.add_line(self.shorten_text(entry))
+            self.paginator.add_line('\u200b')  # only changed thing from super >.>
+
+
 dreamer = commands.Bot(
     command_prefix="!",
     description=description,
     intents=intents,
     allowed_mentions=discord.AllowedMentions().none(),
+    help_command=MyHelpCommand()
 )
 
 
@@ -32,6 +57,6 @@ async def on_ready():
 
 
 dreamer.add_cog(quote_unroll.QuoteUnroll(dreamer))
-dreamer.add_cog(teleport.Teleport(dreamer))
+dreamer.add_cog(utilities.Utilities(dreamer))
 dreamer.run(BOT_TOKEN)
 bot.instance = dreamer
--- utilities.py
+++ utilities.py
@@ -1,29 +1,66 @@
+import re
 import discord
 import validators
 from typing import Optional, Union
 from discord.ext import commands
-from bot import get_bot
 
 
-def isnt_me(message: discord.Message):
-    dreamer = get_bot()
-    if message.author.id != dreamer.user.id:
-        return False
-    else:
-        return True
+class Utilities(commands.Cog):
+    def __init__(self, dreamer):
+        self.dreamer = dreamer
+
+    @commands.command(name="teleport", aliases=["tp"])
+    async def teleport(self, ctx, arg: str):
+        """
+        Use this command to nudge discussion to a different channel.
+        `!tp [channel]`
+        (You must use a channel hyperlink of a channel that exists in the server the command is used in.)
+        """
+        try:
+            portal_exit_channel = self.dreamer.get_channel(
+                int(re.sub("\D", "", arg))
+            )
+            if portal_exit_channel == ctx.channel:
+                await mark_command_invalid(ctx)
+                return
+        except ValueError:
+            await mark_command_invalid(ctx)
+            return
+        portal_entrance = await ctx.send(teleport_to(ctx, arg))
+        portal_exit = await portal_exit_channel.send(teleport_from(ctx))
+        await portal_entrance.edit(
+            content=portal_entrance.content + "\n" + portal_exit.jump_url
+        )
+        await portal_exit.edit(
+            content=portal_exit.content + "\n" + portal_entrance.jump_url
+        )
+
+
+def teleport_from(ctx):
+    message = (
+        "Teleport from: "
+        + ctx.message.channel.mention
+        + ", cast by "
+        + str(ctx.message.author)
+        + ":"
+    )
+    return message
+
+
+def teleport_to(ctx, arg):
+    message = (
+        "Teleport to: " + arg + ", cast by " + str(ctx.message.author) + ":"
+    )
+    return message
+
+
+class Teleport(commands.Cog, name="Banana"):
+    def __init__(self, dreamer):
+        self.dreamer = dreamer
 
 
 def get_data_channel(guild: discord.Guild) -> discord.TextChannel:
     return discord.utils.get(guild.channels, name="bot-data")
-
-
-@commands.bot_has_permissions(manage_messages=True)
-async def clear_data_channel(channel: discord.TextChannel):
-    # I can't really prevent others from using this channel, but I can at least clear out their shit.
-    try:
-        await channel.purge(limit=100, check=isnt_me)
-    except discord.Forbidden:
-        pass
 
 
 async def upsert_shitty_db(
